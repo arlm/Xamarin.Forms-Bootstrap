adfc103f23b9acdb81fb86c4e380d7dae72b0afa
Alexandre Rocha Lima e Marcondes <alexandre.marcondes@gmail.com>
2017-04-11 07:27:56 +0200

    Initial commit

---
+++ App.Android/MainActivity.cs
@@ -0,0 +1,31 @@
+﻿
+using Android.App;
+using Android.Content.PM;
+using Android.OS;
+
+using Xamarin.Forms;
+
+namespace App.Droid
+{
+	[Activity (
+		Label = "swagger_integration",
+		Icon = "@drawable/icon", 
+		Theme="@style/MainTheme",
+		MainLauncher = true,
+		ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation
+	)]
+	public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+	{
+		protected override void OnCreate (Bundle savedInstanceState)
+		{
+			TabLayoutResource = Resource.Layout.Tabbar;
+			ToolbarResource = Resource.Layout.Toolbar; 
+
+			base.OnCreate (savedInstanceState);
+
+			Forms.Init (this, savedInstanceState);
+			LoadApplication (new Shared.Core.App ());
+		}
+	}
+}
+
---
+++ App.Android/MainApplication.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Android.App;
+using Android.Runtime;
+
+namespace App.Droid
+{
+	[Application(
+#if DEBUG
+		Debuggable = true,
+#else
+		Debuggable = false,
+#endif
+		Name = "Android App",
+		Label = "Android App",
+		Description = "Android App description",
+		Icon = "@drawable/icon",
+		RoundIcon = "@drawable/icon",
+		Logo = "@drawable/icon",
+		Theme = "@style/MainTheme"
+	)]
+	public class MainApplication : Application
+	{
+		public MainApplication(IntPtr javaReference, JniHandleOwnership transfer) 
+			: base(javaReference, transfer)
+		{
+			
+		}
+
+		public override void OnCreate()
+		{
+			base.OnCreate();
+		}
+
+		public override void OnTerminate()
+		{
+			base.OnTerminate();
+		}
+
+		public override void OnTrimMemory(Android.Content.TrimMemory level)
+		{
+			base.OnTrimMemory(level);
+		}
+
+		public override void OnLowMemory()
+		{
+			base.OnLowMemory();
+		}
+	}
+}
